23dd337132bceffe6d2c366bf37817801779d082
Jonathan Jin <jjin082693@gmail.com>
2017-08-19 02:54:57 -0400

    net.DimensionException: handle multiple kernel shapes

--- net.py
+++ net.py
@@ -1,28 +1,29 @@
 import numpy as np
 
 class DimensionException(Exception):
-    def __init(self, dim1, dim2):
-        self.dim1 = dim1
-        self.dim2 = dim2
+    def __init(self, shape_patch=None, shapes_kernel=None):
+        self.shape_patch = shape_patch
+        self.shapes_kernel= shapes_kernel
 
     def __str__(self):
-        return repr([self.dim1, self.dim2])
+        return repr([self.shape_patch, self.shapes_kernel])
     
 def convolve(patch_in, *kernels):
-    if len(set([k.shape for k in kernels])) != 1:
-        raise DimensionException(kernels[0].shape)
+    shapes_kernel = [k.shape for k in kernels]
+    if len(set(shapes_kernel)) != 1:
+        raise DimensionException(None, shapes_kernel)
 
     k_shape = kernels[0].shape
 
-    dims_out = [patch_in.shape[dim] - (k_shape[dim]-1) for dim in [0,1]]
-    if (any([d<=0 for d in dims_out])
-        or any(dims_out[d] > patch_in.shape[d] for d in [0,1])
+    shape_out = [patch_in.shape[dim] - (k_shape[dim]-1) for dim in [0,1]]
+    if (any([d<=0 for d in shape_out])
+        or any(shape_out[d] > patch_in.shape[d] for d in [0,1])
         or patch_in.shape[2] != k_shape[2]
     ):
-        raise DimensionException(patch_in.shape, k_shape)
+        raise DimensionException(patch_in.shape, shapes_kernel)
 
-    return np.concatenate([np.array(
-        [[[np.sum(
-            patch_in[i:i+kernel.shape[0],j:j+kernel.shape[1]] * kernel
-        )] for j in range(dims_out[1])] for i in range(dims_out[0])]) for kernel in kernels],
-                          axis=2)
+    return np.concatenate([
+        [[[np.sum(patch_in[i:i+kernel.shape[0],j:j+kernel.shape[1]] * kernel)
+        ] for j in range(shape_out[1])
+        ] for i in range(shape_out[0])
+        ] for kernel in kernels], axis=2)
