4c3e1f87c67cdad482682eb136a68cd7f48419ff
Jonathan Jin <jjin082693@gmail.com>
2017-08-19 02:46:57 -0400

    net.convolve: Handle multi-kernel case

--- net.py
+++ net.py
@@ -7,17 +7,22 @@ class DimensionException(Exception):
 
     def __str__(self):
         return repr([self.dim1, self.dim2])
-        
-def convolve(patch_in, kernel):
-    dims_out = [patch_in.shape[dim] - (kernel.shape[dim]-1) for dim in [0,1]]
+    
+def convolve(patch_in, *kernels):
+    if len(set([k.shape for k in kernels])) != 1:
+        raise DimensionException(kernels[0].shape)
+
+    k_shape = kernels[0].shape
+
+    dims_out = [patch_in.shape[dim] - (k_shape[dim]-1) for dim in [0,1]]
     if (any([d<=0 for d in dims_out])
         or any(dims_out[d] > patch_in.shape[d] for d in [0,1])
-        or patch_in.shape[2] != kernel.shape[2]
+        or patch_in.shape[2] != k_shape[2]
     ):
-        raise DimensionException(patch_in.shape, kernel.shape)
+        raise DimensionException(patch_in.shape, k_shape)
 
-    return np.array([[
-        [np.sum(
+    return np.concatenate([np.array(
+        [[[np.sum(
             patch_in[i:i+kernel.shape[0],j:j+kernel.shape[1]] * kernel
-        )] for j in range(dims_out[1])
-    ] for i in range(dims_out[0])])
+        )] for j in range(dims_out[1])] for i in range(dims_out[0])]) for kernel in kernels],
+                          axis=2)
