200e7f75eaef409e4ba0e719546611f3d665f78e
Jonathan Jin <jjin082693@gmail.com>
2018-04-28 10:22:05 -0400

    Begin literate setup

--- rainy_image_input.py
+++ rainy_image_input.py
@@ -5,57 +5,6 @@ import tensorflow as tf
 GROUND_TRUTH_DIR = "ground truth"
 RAINY_IMAGE_DIR = "rainy image"
 
-
-# Process images of this size. Note that this value differs from the original
-# image sizes, which are either ~300x800 or ~800x300.
-IMAGE_SIZE = 310
-
-
-def dataset(data_dir, batch_size, indices=None):
-    """Construct dataset for rainy-image evaluation.
-
-    Args:
-    data_dir: Path to the data directory.
-    batch_size: Number of images per batch.
-    indices: The input-output pairings to return. If None (the default), uses
-    indices present in the data directory.
-
-    Returns:
-    dataset: Dataset of input-output images.
-    """
-
-    if not indices:
-        indices = _get_indices(data_dir)
-
-    fs_in = _get_input_files(data_dir, indices)
-    fs_out = _get_output_files(data_dir, indices)
-
-    ins = [
-        fname for k, v in iter(sorted(fs_in.iteritems()))
-        for fname in v if k in indices
-    ]
-
-    outs = [v for sublist in [
-        [fname] * len(fs_in[k])
-        for k, fname in iter(sorted(fs_out.iteritems()))
-        if k in indices
-    ] for v in sublist]
-
-    def _parse_function(fname_in, fname_out):
-        def _decode_resize(fname):
-            return tf.image.resize_image_with_crop_or_pad(
-                tf.image.decode_jpeg(fname), IMAGE_SIZE, IMAGE_SIZE,
-            )
-
-        return _decode_resize(fname_in), _decode_resize(fname_out)
-
-    dataset = tf.data.Dataset.from_tensor_slices(
-        (tf.constant(ins), tf.constant(outs)),
-    ).map(_parse_function)
-
-    return dataset.batch(batch_size)
-
-
 def _get_indices(data_dir):
     """Get indices for input/output association.
 
@@ -76,7 +25,6 @@ def _get_indices(data_dir):
         os.path.splitext(os.path.basename(f))[0]
         for f in glob.glob(os.path.join(data_dir, GROUND_TRUTH_DIR, "*.jpg"))
     ]
-
 
 def _get_input_files(data_dir, indices):
     """Get input files from indices.
@@ -125,3 +73,49 @@ def _get_output_files(data_dir, indices):
     return {
         i: os.path.join(directory, "{}.jpg".format(i)) for i in indices
     }
+
+IMAGE_SIZE = 384
+
+def dataset(data_dir, batch_size, indices=None):
+    """Construct dataset for rainy-image evaluation.
+
+    Args:
+    data_dir: Path to the data directory.
+    batch_size: Number of images per batch.
+    indices: The input-output pairings to return. If None (the default), uses
+    indices present in the data directory.
+
+    Returns:
+    dataset: Dataset of input-output images.
+    """
+
+    if not indices:
+        indices = _get_indices(data_dir)
+
+    fs_in = _get_input_files(data_dir, indices)
+    fs_out = _get_output_files(data_dir, indices)
+
+    ins = [
+        fname for k, v in iter(sorted(fs_in.iteritems()))
+        for fname in v if k in indices
+    ]
+
+    outs = [v for sublist in [
+        [fname] * len(fs_in[k])
+        for k, fname in iter(sorted(fs_out.iteritems()))
+        if k in indices
+    ] for v in sublist]
+
+    def _parse_function(fname_in, fname_out):
+        def _decode_resize(fname):
+            return tf.image.resize_image_with_crop_or_pad(
+                tf.image.decode_jpeg(fname), IMAGE_SIZE, IMAGE_SIZE,
+            )
+
+        return _decode_resize(fname_in), _decode_resize(fname_out)
+
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.constant(ins), tf.constant(outs)),
+    ).map(_parse_function)
+
+    return dataset.batch(batch_size)
