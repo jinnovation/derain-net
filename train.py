d208ec8aad6adb1a45b91d9c7339f2cc7cd424a3
Jonathan Jin <jjin082693@gmail.com>
2018-05-01 07:37:03 -0400

    Clarified appendix

--- train.py
+++ train.py
@@ -1,11 +1,6 @@
 import tensorflow as tf
 import logging
 
-LEVEL = tf.logging.DEBUG
-FLAGS = tf.app.flags.FLAGS
-
-logging.basicConfig(level=LEVEL)
-
 tf.app.flags.DEFINE_string("checkpoint_dir", "/tmp/derain-checkpoint",
                            """Directory to write event logs and checkpointing
                            to.""")
@@ -18,11 +13,14 @@ tf.app.flags.DEFINE_integer("batch_size",
                             128,
                             """Number of images to process in a batch.""")
 
-tf.app.flags.DEFINE_integer(
-    "max_steps",
-    1000000,
-    """Number of training batches to run.""",
-)
+tf.app.flags.DEFINE_integer("max_steps",
+                            1000000,
+                            """Number of training batches to run.""")
+
+LEVEL = tf.logging.DEBUG
+FLAGS = tf.app.flags.FLAGS
+
+logging.basicConfig(level=LEVEL)
 
 log = logging.getLogger("derain-train")
 
@@ -46,12 +44,12 @@ def main(argv=None):
     if tf.gfile.Exists(FLAGS.checkpoint_dir):
         log.debug("Emptying checkpoint dir")
         tf.gfile.DeleteRecursively(FLAGS.checkpoint_dir)
+
     log.debug("Creating checkpoint dir")
     tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
 
     train()
 
-
 if __name__ == "__main__":
-    log.info("FLAGS: {}".format(FLAGS.max_steps))
+    log.info("FLAGS: {}".format(FLAGS))
     tf.app.run(main)
